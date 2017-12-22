2bb356fad3fdfc6ed2608e062cd895075919e0d7
James <james.wall@wustl.edu>
2017-12-21 21:07:48 -0800

    got just text

--- FirstWebScraperMain.py
+++ FirstWebScraperMain.py
@@ -1,10 +1,13 @@
 import requests
-
 from BeautifulSoup import BeautifulSoup
 
-url = 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s'
+url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
 response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-print soup.prettify()
+table = soup.find('tbody', attrs={'class': 'stripe'})
+
+for row in table.findAll('tr'):
+    for cell in row.findAll('td'):
+        print cell.text
